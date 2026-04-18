104dc2500378ac45955de1cae677a617c94d3934
Raimey Owens <RO50913@trnty.edu>
2026-04-18 04:55:18 +0000

    Finished athletics page

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 $(document).ready(function(){
+
   // Initialize Tooltip
   $('[data-toggle="tooltip"]').tooltip(); 
   
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+  // Smooth scrolling
+  $(".navbar a, footer a[href='#athleticsPage']").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-
-      // Prevent default anchor click behavior
       event.preventDefault();
-
-      // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 900, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    }
   });
-})
+
+  // ✅ CLEAR FORM ON SUBMIT (FIXED)
+  $("#contactForm").on("submit", function(event) {
+    event.preventDefault(); // stop refresh
+    this.reset(); // clear inputs
+  });
+
+});
